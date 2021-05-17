64c33ea395a32c14d6b7c90d7f837e9abb2efdf1
Sohye Lee <61291530+sohye-lee@users.noreply.github.com>
2021-05-16 22:45:39 -0400

    flask and pipenv

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('StockNews')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 app.run(host="127.0.0.1", port=8080)
